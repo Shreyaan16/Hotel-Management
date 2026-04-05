de56f9868a40425e003ada2c0154e68050211523
Shreyaan <shreyaan.l23@iiits.in>
2026-04-05 15:12:04 +0530

    model training done

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -1,6 +1,15 @@
-from configuration.paths_config import (CONFIG_PATH, TRAIN_FILE_PATH, TEST_FILE_PATH, PROCESSED_DIR)
+from configuration.paths_config import (
+	CONFIG_PATH,
+	TRAIN_FILE_PATH,
+	TEST_FILE_PATH,
+	PROCESSED_DIR,
+	PROCESSED_TRAIN_DATA_PATH,
+	PROCESSED_TEST_DATA_PATH,
+	MODEL_OUTPUT_PATH,
+)
 from src.components.data_ingestion import DataIngestion
 from src.components.data_preprocessing import DataProcessor
+from src.components.model_training import ModelTraining
 from src.utils import read_yaml
 
 def run_pipeline() -> None:
@@ -13,6 +22,9 @@ def run_pipeline() -> None:
 
 	processor = DataProcessor(TRAIN_FILE_PATH, TEST_FILE_PATH, PROCESSED_DIR, CONFIG_PATH)
 	processor.process()
+
+	trainer = ModelTraining(PROCESSED_TRAIN_DATA_PATH, PROCESSED_TEST_DATA_PATH, MODEL_OUTPUT_PATH)
+	trainer.run()
 
 
 if __name__ == "__main__":
