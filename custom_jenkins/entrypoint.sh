9b62f6ae414282605b9e54ed211bc21a86d79e63
Shreyaan <shreyaan.l23@iiits.in>
2026-04-06 04:32:52 +0530

    acr push

--- custom_jenkins/entrypoint.sh
+++ custom_jenkins/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Fix docker socket GID mismatch at runtime
-DOCKER_SOCK_GID=$(stat -c '%g' /var/run/docker.sock)
-CURRENT_GID=$(getent group docker | cut -d: -f3)
+set -e
 
-if [ "$DOCKER_SOCK_GID" != "$CURRENT_GID" ]; then
+# Fix docker socket GID mismatch at runtime
+if [ -S /var/run/docker.sock ]; then
+    DOCKER_SOCK_GID=$(stat -c '%g' /var/run/docker.sock)
     groupmod -g "$DOCKER_SOCK_GID" docker
+    usermod -aG docker jenkins
 fi
 
-exec /usr/bin/tini -- /usr/local/bin/jenkins.sh "$@"
+# Drop privileges and start Jenkins as jenkins user
+exec gosu jenkins /usr/bin/tini -- /usr/local/bin/jenkins.sh "$@"
