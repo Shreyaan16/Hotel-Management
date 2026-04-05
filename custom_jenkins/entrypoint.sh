8dfb2c0edd30ae79562998d0e25a85268f68d41f
Shreyaan <shreyaan.l23@iiits.in>
2026-04-06 04:37:41 +0530

    acr

--- custom_jenkins/entrypoint.sh
+++ custom_jenkins/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-# Fix docker socket GID mismatch at runtime
+# Fix docker socket permissions at runtime
 if [ -S /var/run/docker.sock ]; then
-    DOCKER_SOCK_GID=$(stat -c '%g' /var/run/docker.sock)
-    groupmod -g "$DOCKER_SOCK_GID" docker
-    usermod -aG docker jenkins
+    chmod 666 /var/run/docker.sock
 fi
 
 # Drop privileges and start Jenkins as jenkins user
